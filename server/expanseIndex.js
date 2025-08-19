25836af98a56cdf1f96610b2ffcd2f02e02111d1
akashkumar-gupta <akashkumargupta560@gmail.com>
2025-08-19 19:17:54 +0530

    first changes

--- server/expanseIndex.js
+++ server/expanseIndex.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://localhost:5173",
   methods: ["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
 
   credentials: true
