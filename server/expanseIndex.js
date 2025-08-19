4e590a1ff9fd36edf2495c674187e876eb3eb15d
akashkumar-gupta <akashkumargupta560@gmail.com>
2025-08-19 18:44:25 +0530

    some changes

--- server/expanseIndex.js
+++ server/expanseIndex.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: [process.env.CLIENT_URL], 
+  origin: ["https://expanse992.onrender.com"], 
   methods: ["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
   credentials: true, 
 }));
