200939ca64ea5a46ebe34444c2fd980bc4a01106
Prince Thakur <princethakurameotech@gmail.com>
2025-08-20 14:15:41 +0530

    loader added

--- client/src/components/protectedRoute/ProtectedRoute.jsx
+++ client/src/components/protectedRoute/ProtectedRoute.jsx
@@ -4,11 +4,11 @@ import { Navigate } from "react-router-dom";
 const ProtectedRoute = ({ children }) => {
   const { token, isLoading } = useSelector((state) => state.Auth);
 
-  if (!token) {
-    return <Navigate to="/" />;
-  }
   if (isLoading) {
     return <div className="text-center py-4 text-red-800 font-bold">Loading,Please wait...</div>;
+  }
+  if (!token) {
+    return <Navigate to="/" />;
   }
   return children;
 };
