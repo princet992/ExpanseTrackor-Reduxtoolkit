5fb8212e72302ac8aec414e7bdb1d01878448b90
Prince Thakur <princethakurameotech@gmail.com>
2025-08-20 18:26:49 +0530

    changes

---
+++ client/src/components/ExpanseSkelton.jsx
@@ -0,0 +1,14 @@
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+const ExpanseSkelton = () => {
+  return (
+    <div className="rounded-2xl shadow-md px-4 py-6">
+      <Skeleton height={24} width={150} className="mb-4" />
+
+      <Skeleton count={3} height={20} className="mb-2" />
+    </div>
+  );
+};
+
+export default ExpanseSkelton;
