fd86499fd072d0c27396eab9c49e205e34c36687
Prince Thakur <princethakurameotech@gmail.com>
2025-08-20 15:50:22 +0530

    header updated

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -58,7 +58,7 @@ const Header = () => {
           <button
             onClick={handleToggleTheme}
             className={`p-1 rounded-full transition shadow-md hover:bg-yellow-300 ${
-              theme === "dark" ? "bg-yellow-400 text-gray-900 hover:bg-yellow-300 hover:text-black" : "bg-white text-blue-600 hover:bg-yellow-300 hover:text-black"
+              theme === "dark" ? "bg-gray-700 hover:bg-gray-600" : "bg-gray-100 hover:bg-gray-200"
             }`}
             title="Toggle Theme"
           >
