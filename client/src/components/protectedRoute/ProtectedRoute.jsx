6ce07895d717c6ed65856c4ca61e991d8309abf2
Prince Thakur <princethakurameotech@gmail.com>
2025-08-20 14:11:19 +0530

    loading added

--- client/src/components/protectedRoute/ProtectedRoute.jsx
+++ client/src/components/protectedRoute/ProtectedRoute.jsx
@@ -2,10 +2,13 @@ import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const { token } = useSelector((state) => state.Auth);
+  const { token, isLoading } = useSelector((state) => state.Auth);
 
   if (!token) {
     return <Navigate to="/" />;
+  }
+  if (isLoading) {
+    return <div className="text-center py-4 text-red-800 font-bold">Loading,Please wait...</div>;
   }
   return children;
 };
