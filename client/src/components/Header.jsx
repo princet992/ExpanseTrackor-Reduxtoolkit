2372056d71cdc5c174e7ebf13c9835e8ca46db59
Prince Thakur <princethakurameotech@gmail.com>
2025-08-20 15:47:57 +0530

    some changes to header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -65,7 +65,7 @@ const Header = () => {
             {theme === "dark" ? (
               <Sun className="text-yellow-300" size={20} />
             ) : (
-              <Moon className="text-gray-900" size={20} />
+              <Moon className="text-gray-900 hover:bg-yellow-300 hover:text-black" size={20} />
             )}
           </button>
 
