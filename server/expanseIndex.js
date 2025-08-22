9f9931527c4549cdd3bfda6c3e64755050b31d26
Prince Thakur <princethakurameotech@gmail.com>
2025-08-22 16:23:56 +0530

    index changed

--- server/expanseIndex.js
+++ server/expanseIndex.js
@@ -9,7 +9,15 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL || "http://localhost:5173/",
+  credentials: true,
+}));
+
+//test
+app.get("/", (req, res) => {
+  res.send("API is working 🚀");
+});
 
 //create transaction
 app.post("/txHistory", createExpanseData);
